0ad4163f8a393c2ba825f67c4c674fc48ff9ed5b
zuhito <zuhito@hotmail.co.jp>
2019-01-27 22:29:34 +0900

    Set 0 to node input

--- julius.js
+++ julius.js
@@ -10,25 +10,23 @@ var arg = ['julius',
     __dirname + '/dict/tjbot'];
 var cmd = arg.shift();
 var child = spawn(cmd, arg);
+var statusMessage = {
+    fill: "blue",
+    shape: "ring",
+    text: "「TJボット」と話しかけてください"
+};
+node.status(statusMessage);
 child.stdout.on('data', function (data) {
     var matcher = data.toString().match(/^pass1_best: \[s\] (.+) \[\/s\]/);
     if (matcher) {
         console.log('pass1_best: ' + matcher[0]);
-        node.status({
-            fill: "blue",
-            shape: "ring",
-            text: "「TJボット」と話しかけてください"
-        });
+        node.status(statusMessage);
         var keyword = matcher[1];
         if (keyword === "TJBot") {
             flow.set("triggerTime", new Date().getTime());
             node.status({fill:"blue", shape: "dot", text: "認識中"});
             var id = setTimeout(function() {
-                node.status({
-                    fill: "blue",
-                    shape: "ring",
-                    text: "「TJボット」と話しかけてください"
-                });
+                node.status(statusMessage);
             }, 10 * 1000);
             flow.set("triggerId", id);
             msg.payload = keyword;
@@ -38,11 +36,7 @@ child.stdout.on('data', function (data) {
             if (0 * 1000 < windowTime && windowTime < 10 * 1000) {
                 flow.set("triggerTime", 0);
                 clearTimeout(flow.get("triggerId"));
-                node.status({
-                    fill: "blue",
-                    shape: "ring",
-                    text: "「TJボット」と話しかけてください"
-                });
+                node.status(statusMessage);
                 msg.payload = keyword;
                 node.send(msg);
             }
