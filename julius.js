b731e27247b895cba6232da0fd087285cae53531
zuhito <zuhito@hotmail.co.jp>
2019-01-28 22:53:20 +0900

    Set ALSADEV to environment variable

--- julius.js
+++ julius.js
@@ -1,5 +1,9 @@
 // name: julius
 // outputs: 1
+var execSync = global.get('child_process').execSync;
+var result =  execSync('export ALSADEV="plughw:1,0"');
+console.log(result.toString());
+
 var spawn = global.get('child_process').spawn;
 var __dirname = global.get('__dirname');
 var arg = ['julius',
@@ -10,45 +14,57 @@ var arg = ['julius',
     __dirname + '/dict/tjbot'];
 var cmd = arg.shift();
 var child = spawn(cmd, arg);
-var statusMessage = {
-    fill: "blue",
-    shape: "ring",
-    text: "「TJボット」と話しかけてください"
-};
-node.status(statusMessage);
+
 child.stdout.on('data', function (data) {
     var matcher = data.toString().match(/^pass1_best: \[s\] (.+) \[\/s\]/);
     if (matcher) {
         console.log('pass1_best: ' + matcher[0]);
-        node.status(statusMessage);
         var keyword = matcher[1];
         if (keyword === "TJBot") {
             flow.set("triggerTime", new Date().getTime());
-            node.status({fill:"blue", shape: "dot", text: "認識中"});
-            var id = setTimeout(function() {
-                node.status(statusMessage);
-            }, 10 * 1000);
-            flow.set("triggerId", id);
             msg.payload = keyword;
             node.send(msg);
         } else {
             var windowTime = new Date().getTime() - flow.get("triggerTime");
             if (0 * 1000 < windowTime && windowTime < 10 * 1000) {
                 flow.set("triggerTime", 0);
-                clearTimeout(flow.get("triggerId"));
-                node.status(statusMessage);
                 msg.payload = keyword;
                 node.send(msg);
             }
         }
     }
 });
+
 child.stderr.on('data', function (data) {
     console.log('stderr: ' + data.toString());
+    if (data.toString() === '<<< please speak >>>') {
+        var triggerTime = flow.get("triggerTime");
+        if (triggerTime && triggerTime !== 0) {
+            node.status({
+                fill:"blue",
+                shape: "dot",
+                text: "認識中"
+            });
+        } else {
+            node.status({
+                fill: "blue",
+                shape: "ring",
+                text: "「TJボット」と話しかけてください"
+            });
+        }
+    } else if (data.toString() !== '') {
+        node.status({
+            fill: "red",
+            shape: "ring",
+            text: data.toString()
+        });
+    }
 });
+
 child.on('close', function (code, signal) {
     console.log('close: ' + code + ', ' + signal);
 });
+
 child.on('error', function (code) {
     console.log('error: ' + code);
 });
